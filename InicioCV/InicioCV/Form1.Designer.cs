79c77b949b134118e6100e979f6cfaeea88672da
Jean Leal <Jean Leal>
2016-01-06 19:32:21 -0300

    alteração nos labels

--- InicioCV/InicioCV/Form1.Designer.cs
+++ InicioCV/InicioCV/Form1.Designer.cs
@@ -32,9 +32,7 @@
             this.captureImageBox = new Emgu.CV.UI.ImageBox();
             this.captureButton = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.captureImageBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // captureImageBox
@@ -47,45 +45,36 @@
             // 
             // captureButton
             // 
-            this.captureButton.Location = new System.Drawing.Point(192, 341);
+            this.captureButton.Location = new System.Drawing.Point(154, 341);
             this.captureButton.Name = "captureButton";
             this.captureButton.Size = new System.Drawing.Size(75, 23);
             this.captureButton.TabIndex = 3;
-            this.captureButton.Text = "button1";
+            this.captureButton.Text = "Iniciar";
             this.captureButton.UseVisualStyleBackColor = true;
             this.captureButton.Click += new System.EventHandler(this.captureButton_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(342, 341);
+            this.button1.Location = new System.Drawing.Point(273, 341);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "Tirar Foto";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(494, 68);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(255, 221);
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(761, 376);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(469, 376);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.captureButton);
             this.Controls.Add(this.captureImageBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.captureImageBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -95,7 +84,6 @@
         private Emgu.CV.UI.ImageBox captureImageBox;
         private System.Windows.Forms.Button captureButton;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
