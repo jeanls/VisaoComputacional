1e75ae3ec6311f0361886a7d3fbb719324b1d4ef
Jean Leal <Jean Leal>
2016-01-06 19:12:44 -0300

    Projeto hello world

---
+++ InicioCV/InicioCV/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace InicioCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.captureImageBox = new Emgu.CV.UI.ImageBox();
+            this.captureButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.captureImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // captureImageBox
+            // 
+            this.captureImageBox.Location = new System.Drawing.Point(12, 12);
+            this.captureImageBox.Name = "captureImageBox";
+            this.captureImageBox.Size = new System.Drawing.Size(450, 323);
+            this.captureImageBox.TabIndex = 2;
+            this.captureImageBox.TabStop = false;
+            // 
+            // captureButton
+            // 
+            this.captureButton.Location = new System.Drawing.Point(192, 341);
+            this.captureButton.Name = "captureButton";
+            this.captureButton.Size = new System.Drawing.Size(75, 23);
+            this.captureButton.TabIndex = 3;
+            this.captureButton.Text = "button1";
+            this.captureButton.UseVisualStyleBackColor = true;
+            this.captureButton.Click += new System.EventHandler(this.captureButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(494, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(255, 221);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 376);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.captureButton);
+            this.Controls.Add(this.captureImageBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.captureImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox captureImageBox;
+        private System.Windows.Forms.Button captureButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ InicioCV/InicioCV/Form1.cs
@@ -0,0 +1,78 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InicioCV
+{
+    public partial class Form1 : Form
+    {
+        private Capture _capture = null;
+        private bool _captureInProgress;
+        private Mat frame;
+        public Form1()
+        {
+            InitializeComponent();
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                _capture = new Capture();
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            frame = new Mat();
+            _capture.Retrieve(frame, 0);
+            captureImageBox.Image = frame;
+        }
+
+        private void takePicture()
+        {
+            Mat print = _capture.QueryFrame();
+            Bitmap img = print.Bitmap;
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                img.Save(dialog.FileName, ImageFormat.Png);
+            }
+        }
+
+        private void captureButton_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                if (_captureInProgress)
+                {  //stop the capture
+                    captureButton.Text = "Start Capture";
+                    _capture.Pause();
+                }
+                else
+                {
+                    //start the capture
+                    captureButton.Text = "Stop";
+                    _capture.Start();
+                }
+
+                _captureInProgress = !_captureInProgress;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            takePicture();
+        }
+    }
+}
